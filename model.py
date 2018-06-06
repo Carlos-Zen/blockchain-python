209ac232ed7fc437c93791c1bc2c851579a0dea0
李智超(奕风) <zhchli@uuzu.com>
2018-06-06 18:07:03 +0800

    seperate model from trans

---
+++ model.py
@@ -0,0 +1,24 @@
+# coding:utf-8
+import json
+
+class Model():
+    
+    def __getitem__(self,key):
+        print(key)
+        if hasattr(self,key):
+            return getattr(self,key)
+        else:
+            return None
+
+    def __setitem__(self,key,value):
+        if hasattr(self,key):
+            setattr(self,key,value)
+        else:
+            pass
+
+    def __str__(self):
+        return str(self.__dict__.items()
+        )
+        
+    def json(self):
+        return json.dumps(self.__dict__)
